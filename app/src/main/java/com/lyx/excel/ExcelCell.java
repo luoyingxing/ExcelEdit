22cdda162be26d9e1bc78d6642515c6be2ff1c57
luoyingxing <luoyingxing@126.com>
2019-01-09 15:09:21 +0800

    Initial commit

---
+++ app/src/main/java/com/lyx/excel/ExcelCell.java
@@ -0,0 +1,10 @@
+package com.lyx.excel;
+
+/**
+ * ExcelCell
+ * <p>
+ * <p/>
+ * Created by luoyingxing on 2019/1/9.
+ */
+public class ExcelCell {
+}
---
+++ app/src/main/java/com/lyx/excel/Info.java
@@ -0,0 +1,27 @@
+package com.lyx.excel;
+
+/**
+ * author:  luoyingxing
+ * date: 2019/1/8.
+ */
+public class Info {
+    public String provider;
+    public String card;
+    public String number;
+    public String information;
+
+    public Info() {
+    }
+
+    public Info(String provider, String card, String number, String information) {
+        this.provider = provider;
+        this.card = card;
+        this.number = number;
+        this.information = information;
+    }
+
+    @Override
+    public String toString() {
+        return provider + "   " + card + "   " + number + "   " + information + "   ";
+    }
+}
